a63113a71db0fdc447ff9bb103bbd82c1ca2c64d
picbidervis <50153843+picbidervis@users.noreply.github.com>
2019-05-25 17:53:46 +0300

    Update hn_submissions.py

--- data_visualization/api/hn_submissions.py
+++ data_visualization/api/hn_submissions.py
@@ -30,3 +30,38 @@ for submission_dict in submission_dicts:
     print("\nTitle: ", submission_dict["title"])
     print("Discussion link: ", submission_dict["link"])
     print("Comments: ", submission_dict["comments"])
+
+
+#Visualation
+
+titles, plot_dicts = [], []
+
+for submission_dict in submission_dicts:
+    titles.append(submission_dict["title"])
+    plot_dict = {
+        'value': submission_dict['comments'],
+        'label': submission_dict['title'],
+        'xlink': submission_dict['link'],
+    }
+    plot_dicts.append(plot_dict)
+
+
+my_style = LS('#333366', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+my_config.y_title = 'Number of Comments'
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most Active Discussions on Hacker News'
+chart.x_labels = titles
+
+chart.add('', plot_dicts)
+chart.render_to_file('hn_discussions.svg')
